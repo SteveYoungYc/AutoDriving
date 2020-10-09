cf9b6d2afd98197b15d6bdf998b542a3083286d4
SteveYoungYc <46893507+SteveYoungYc@users.noreply.github.com>
2020-10-09 21:47:02 +0800

    识别直线

--- Final.py
+++ Final.py
@@ -10,14 +10,20 @@ canny_hthreshold = 150  # Canny edge detection high threshold
 rho = 1
 theta = np.pi / 180
 threshold = 15
-min_line_length = 40
-max_line_gap = 20
+min_line_length = 200
+max_line_gap = 3.2
+
+# roi_vtx = np.array([[(0, 720), (0, 380), (1280, 380), (1280, 720), (1050, 720), (780, 440), (500, 440), (230, 720)]])
+roi_vtx = np.array([[(0, 720), (0, 380), (1280, 380), (1280, 720), (1050, 720), (780, 440), (500, 440), (230, 720),
+                     (230, 380), (1050, 380)]])
+ymin = 380
+ymax = 720
 
 
 def roi_mask(img, vertices):
     mask = np.zeros_like(img)
 
-    if len(img.shape) > 2:
+    if len(img.shape) >= 3:
         channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
         mask_color = (255,) * channel_count
     else:
@@ -33,15 +39,17 @@ def draw_roi(img, vertices):
 
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
-    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), min_line_len,
-                            max_line_gap)
-    line_img = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
-
-    draw_lanes(line_img, lines)
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
+                            maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    try:
+        draw_lanes(line_img, lines)
+    except:
+        pass
     return line_img
 
 
-def draw_lanes(img, lines, color=[0, 0, 255], thickness=8):
+def draw_lanes(img, lines, color=(255, 0, 0), thickness=8):
     left_lines, right_lines = [], []
     for line in lines:
         for x1, y1, x2, y2 in line:
@@ -51,18 +59,19 @@ def draw_lanes(img, lines, color=[0, 0, 255], thickness=8):
             else:
                 right_lines.append(line)
 
-    if (len(left_lines) <= 0 or len(right_lines) <= 0):
+    if len(left_lines) == 0 or len(right_lines) == 0:
         return img
 
     clean_lines(left_lines, 0.1)
     clean_lines(right_lines, 0.1)
-    left_points = [(x1, y1) for line in left_lines for x1, y1, x2, y2 in line]
-    left_points = left_points + [(x2, y2) for line in left_lines for x1, y1, x2, y2 in line]
-    right_points = [(x1, y1) for line in right_lines for x1, y1, x2, y2 in line]
-    right_points = right_points + [(x2, y2) for line in right_lines for x1, y1, x2, y2 in line]
 
-    left_vtx = calc_lane_vertices(left_points, 325, img.shape[0])
-    right_vtx = calc_lane_vertices(right_points, 325, img.shape[0])
+    left_points = [(x1, y1) for line in left_lines for x1, y1, _, _ in line]
+    left_points = left_points + [(x2, y2) for line in left_lines for _, _, x2, y2 in line]
+    right_points = [(x1, y1) for line in right_lines for x1, y1, _, _ in line]
+    right_points = right_points + [(x2, y2) for line in right_lines for _, _, x2, y2 in line]
+
+    left_vtx = calc_lane_vertices(left_points)
+    right_vtx = calc_lane_vertices(right_points)
 
     cv2.line(img, left_vtx[0], left_vtx[1], color, thickness)
     cv2.line(img, right_vtx[0], right_vtx[1], color, thickness)
@@ -81,7 +90,7 @@ def clean_lines(lines, threshold):
             break
 
 
-def calc_lane_vertices(point_list, ymin, ymax):
+def calc_lane_vertices(point_list):
     x = [p[0] for p in point_list]
     y = [p[1] for p in point_list]
     fit = np.polyfit(y, x, 1)
@@ -90,18 +99,15 @@ def calc_lane_vertices(point_list, ymin, ymax):
     xmin = int(fit_fn(ymin))
     xmax = int(fit_fn(ymax))
 
-    return [(xmin, ymin), (xmax, ymax)]
+    return ((xmin, ymin), (xmax, ymax))
 
 
 def process_an_image(img):
-    roi_vtx = np.array([[(0, img.shape[0]), (460, 325), (520, 325), (img.shape[1], img.shape[0])]])
-
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # 灰值
-    blur_gray = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 0, 0)  # 高斯平滑
-    edges = cv2.Canny(blur_gray, canny_lthreshold, canny_hthreshold)  # canny边缘
-    roi_edges = roi_mask(edges, roi_vtx)  # 兴趣区间
-    line_img = hough_lines(roi_edges, rho, theta, threshold, min_line_length, max_line_gap)  # hough变换
-    res_img = cv2.addWeighted(img, 0.8, line_img, 1, 0)  # 加到原图上
+    blur_gray = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), (blur_ksize, blur_ksize), 0, 0)
+    edges = cv2.Canny(blur_gray, canny_lthreshold, canny_hthreshold)
+    roi_edges = roi_mask(edges, roi_vtx)
+    line_img = hough_lines(roi_edges, rho, theta, threshold, min_line_length, max_line_gap)
+    res_img = cv2.addWeighted(img, 0.8, line_img, 1, 0)
 
     return res_img
 
@@ -113,17 +119,10 @@ clip = VideoFileClip("D:\\SJTU\\Auto Driving\\20200618 124512_outVideo.avi")  # 
 out_clip = clip.fl_image(process_an_image)  # 对视频的每一帧进行处理
 out_clip.write_videofile(output, audio=True)  # 将处理后的视频写入新的视频文件
 
-# 一张图片
-
-img = cv2.imread("D:\\SJTU\\Auto Driving\\Lane.jpg")
-res_img = process_an_image(img)
-cv2.imshow('A1', img)
-cv2.imshow('A2', res_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 # 实时显示
-cap = cv2.VideoCapture("D:\\SJTU\\Auto Driving\\20200618 124512_outVideo.avi")  # 指定路径读取视频。如果cv2.VideoCapture(0)，没有指定路径，则从电脑自带摄像头取视频。
+cap = cv2.VideoCapture(
+    "D:\\SJTU\\Auto Driving\\20200618 124512_outVideo.avi")  # 指定路径读取视频。如果cv2.VideoCapture(0)，没有指定路径，则从电脑自带摄像头取视频。
 ret = True
 # ret,frame = cap.read()
 while ret:
